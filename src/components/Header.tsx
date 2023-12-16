3ce58365cf51c53781f040e390c5d5d617bfd413
Keith Cabay <keith.cabay10@gmail.com>
2023-12-16 18:05:02 -0500

    Update header with new logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,39 @@
 import { Box, HStack, Text, Image } from "@chakra-ui/react";
-import Airplane from "../components/images/airplane.png";
+import Icon from "../components/images/icon_black.png";
 import { Link as ReactRouterLink } from "react-router-dom";
 import { Link as ChakraLink } from "@chakra-ui/react";
 
 const Header = () => {
   return (
-    <Box p="10px 30px 10px 30px" boxShadow="base">
-      <HStack>
-        <HStack>
-          <Image
-            src={Airplane}
-            w="80px"
-            mt="20px"
-            style={{ transform: "rotate(-10deg" }}
-          />
-        </HStack>
+    <Box boxShadow="base">
+      <HStack
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        p="10px 25px 10px 25px"
+      >
+        <ChakraLink as={ReactRouterLink} to="/home">
+          <HStack>
+            <Image src={Icon} w="60px" />
+          </HStack>
+        </ChakraLink>
+
         <ChakraLink
           as={ReactRouterLink}
           to="/home"
-          pl="115px"
-          ml="auto"
           fontWeight="bold"
           fontSize="25px"
+          pl="120px"
           _hover={{ color: "#b3bab4" }}
         >
           Wander
         </ChakraLink>
-        <HStack marginLeft="auto">
+        <HStack>
           <Text fontSize="15px">Forum</Text>
-          <Text fontSize="15px" pl="10px">
+          <Text fontSize="15px" pl="15px">
             Start New
           </Text>
-          <Text fontSize="15px" pl="10px">
+          <Text fontSize="15px" pl="15px">
             Log In
           </Text>
         </HStack>
