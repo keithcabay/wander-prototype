5c15de56168e4fff94d68ff98e178f1ce0913c92
Keith Cabay <keith.cabay10@gmail.com>
2023-12-17 17:33:58 -0500

    Add functionality to map(map position matches selected location)

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -12,11 +12,12 @@ import {
   Td,
 } from "@chakra-ui/react";
 import { useState } from "react";
+import dateActivity, { Day } from "./Data";
+import MapSchedule from "../components/MapSchedule";
 
-interface Day {
-  time: string;
-  description: string;
-  location: string;
+interface Location {
+  lat: number;
+  lng: number;
 }
 
 const dates = [
@@ -24,191 +25,26 @@ const dates = [
   23, 24, 25, 26, 27, 28, 29, 30,
 ];
 
-const dateActivity: Day[][] = [
-  [
-    {
-      time: "4:00",
-      description: "Board flight to Paris, France",
-      location: "J.F.K Airport",
-    },
-    {
-      time: "17:00",
-      description: "Arrive in Paris, France",
-      location: "C.D.G Airport",
-    },
-    {
-      time: "19:30",
-      description: "Get to hotel: Pullman Paris",
-      location: "18 Avenue De Suffren, 22 Rue Jean Rey Entree Au",
-    },
-    {
-      time: "20:30",
-      description: "Eat Dinner: Frame Restaurant",
-      location: "18 Avenue De Suffren, 22 Rue Jean Rey Entree Au",
-    },
-  ],
-  [
-    {
-      time: "7:00",
-      description: "Get Breakfast at Aux Cerises",
-      location: "47 Av. De Suffren, 75007",
-    },
-    {
-      time: "9:30",
-      description:
-        "Explore Trocadero Platform and take pictures with the Eiffel Tower",
-      location: "Pl. du Trocadero et du 11 Novembre",
-    },
-    {
-      time: "11:30",
-      description: "Take a cab to the Arc de Triomphe",
-      location: " ",
-    },
-    {
-      time: "12:00",
-      description: "Explore the Arc de Triomphe",
-      location: "Arc de Triomphe, Pl. Charles de Gaulle",
-    },
-    {
-      time: "14:00",
-      description: "Lunch at Les Cocottes",
-      location: "135 Rue Saint-Dominique, 75507 Paris",
-    },
-    {
-      time: "16:00",
-      description: "Explore and take pictures at Hotel des Invalides",
-      location: "Hotel des Invalides, 75007 Paris",
-    },
-    {
-      time: "19:00",
-      description: "To the top of the Eiffel Tower",
-      location: "Eiffel Tower, Champ de Mars, 5 Av. Anatole France",
-    },
-    {
-      time: "21:00",
-      description: "Eat dinner at the Eiffel Tower: Jules Verne",
-      location: "Avenue Gustave Eiffel 2eme, Eiffel Tower, Av. Anatole France",
-    },
-  ],
-  [
-    {
-      time: "4:00",
-      description: "Board flight to Paris, France",
-      location: "J.F.K Airport",
-    },
-    {
-      time: "17:00",
-      description: "Arrive in Paris, France",
-      location: "C.D.G Airport",
-    },
-    {
-      time: "19:30",
-      description: "Get to hotel: Pullman Paris",
-      location: "18 Avenue De Suffren, 22 Rue Jean Rey Entree Au",
-    },
-    {
-      time: "20:30",
-      description: "Eat Dinner: Frame Restaurant",
-      location: "18 Avenue De Suffren, 22 Rue Jean Rey Entree Au",
-    },
-  ],
-  [
-    {
-      time: "4:00",
-      description: "Board flight to Paris, France",
-      location: "J.F.K Airport",
-    },
-    {
-      time: "17:00",
-      description: "Arrive in Paris, France",
-      location: "C.D.G Airport",
-    },
-    {
-      time: "19:30",
-      description: "Get to hotel: Pullman Paris",
-      location: "18 Avenue De Suffren, 22 Rue Jean Rey Entree Au",
-    },
-    {
-      time: "20:30",
-      description: "Eat Dinner: Frame Restaurant",
-      location: "18 Avenue De Suffren, 22 Rue Jean Rey Entree Au",
-    },
-  ],
-  [
-    {
-      time: "4:00",
-      description: "Board flight to Paris, France",
-      location: "J.F.K Airport",
-    },
-    {
-      time: "17:00",
-      description: "Arrive in Paris, France",
-      location: "C.D.G Airport",
-    },
-    {
-      time: "19:30",
-      description: "Get to hotel: Pullman Paris",
-      location: "18 Avenue De Suffren, 22 Rue Jean Rey Entree Au",
-    },
-    {
-      time: "20:30",
-      description: "Eat Dinner: Frame Restaurant",
-      location: "18 Avenue De Suffren, 22 Rue Jean Rey Entree Au",
-    },
-  ],
-  [
-    {
-      time: "4:00",
-      description: "Board flight to Paris, France",
-      location: "J.F.K Airport",
-    },
-    {
-      time: "17:00",
-      description: "Arrive in Paris, France",
-      location: "C.D.G Airport",
-    },
-    {
-      time: "19:30",
-      description: "Get to hotel: Pullman Paris",
-      location: "18 Avenue De Suffren, 22 Rue Jean Rey Entree Au",
-    },
-    {
-      time: "20:30",
-      description: "Eat Dinner: Frame Restaurant",
-      location: "18 Avenue De Suffren, 22 Rue Jean Rey Entree Au",
-    },
-  ],
-  [
-    {
-      time: "4:00",
-      description: "Board flight to Paris, France",
-      location: "J.F.K Airport",
-    },
-    {
-      time: "17:00",
-      description: "Arrive in Paris, France",
-      location: "C.D.G Airport",
-    },
-    {
-      time: "19:30",
-      description: "Get to hotel: Pullman Paris",
-      location: "18 Avenue De Suffren, 22 Rue Jean Rey Entree Au",
-    },
-    {
-      time: "20:30",
-      description: "Eat Dinner: Frame Restaurant",
-      location: "18 Avenue De Suffren, 22 Rue Jean Rey Entree Au",
-    },
-  ],
-];
-
 const Calendar = () => {
   const [currentDayActivity, setCurrentDayActivity] = useState<Day[][]>([[]]);
-  const [selectedDate, setSelectedDate] = useState(15);
+  const [selectedDate, setSelectedDate] = useState(14);
+  const [currLocation, setCurrLocation] = useState<Location>({
+    lat: 48.85997,
+    lng: 2.34678,
+  });
 
   const handleOnClick = (index: number) => {
     setSelectedDate(index);
     setCurrentDayActivity([dateActivity[index - 14]]);
+  };
+
+  const handleLocationClick = (day: number, activity: number) => {
+    const currActivity = dateActivity[day][activity];
+
+    setCurrLocation({
+      lat: currActivity.latitude,
+      lng: currActivity.longitude,
+    });
   };
 
   return (
@@ -241,9 +77,9 @@ const Calendar = () => {
         })}
       </Box>
       <Box>
-        <TableContainer>
+        <TableContainer w="1200px">
           <Table>
-            <TableCaption>{selectedDate}</TableCaption>
+            <TableCaption>March {selectedDate + 1}th</TableCaption>
             <Thead>
               <Tr>
                 <Th>Time</Th>
@@ -260,13 +96,28 @@ const Calendar = () => {
                       <Tr key={index}>
                         <Td>{time}</Td>
                         <Td>{description}</Td>
-                        <Td>{location}</Td>
+                        <Td
+                          onClick={() =>
+                            handleLocationClick(selectedDate - 14, index)
+                          }
+                        >
+                          {location}
+                        </Td>
                       </Tr>
                     ))
                 )}
             </Tbody>
           </Table>
         </TableContainer>
+        {selectedDate >= 14 && selectedDate < 21 ? (
+          <MapSchedule
+            lat={currLocation.lat}
+            lng={currLocation.lng}
+            day={selectedDate - 14}
+          />
+        ) : (
+          <></>
+        )}
       </Box>
     </VStack>
   );
