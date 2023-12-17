c8c78e2cc6e33c1808ba48d2d0a712c76c38bbd9
Keith Cabay <keith.cabay10@gmail.com>
2023-12-17 17:35:14 -0500

    Match map position with selected position and added pins for all locations

--- src/components/MapSchedule.tsx
+++ src/components/MapSchedule.tsx
@@ -1,6 +1,6 @@
-import { Box, Text, Flex } from "@chakra-ui/react";
+import { Box, Text, Flex, Hide } from "@chakra-ui/react";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   APIProvider,
   Map,
@@ -9,29 +9,57 @@ import {
   InfoWindow,
 } from "@vis.gl/react-google-maps";
 
-const MapSchedule = () => {
+import dateActivity, { Day } from "./Data";
+
+interface Props {
+  lat: number;
+  lng: number;
+  day: number;
+}
+
+const MapSchedule = ({ lat, lng, day }: Props) => {
   const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
   const MAP_KEY = import.meta.env.VITE_REACT_APP_MAP_KEY;
 
-  const position = { lat: 48.85, lng: 2.35 };
-  const [open, setOpen] = useState(false);
+  //const [open, setOpen] = useState(false);
+  const [position, setPosition] = useState({ lat, lng });
+  const [currentDay, setCurrentDay] = useState(dateActivity[day]);
+
+  useEffect(() => {
+    setCurrentDay(dateActivity[day]);
+  }, [day]);
+
+  useEffect(() => {
+    setPosition({ lat, lng });
+  }, [lat, lng]);
 
   return (
     <APIProvider apiKey={API_KEY}>
       <Flex justifyContent="center">
-        <Box h="500px" w="500px">
+        <Box h="500px" w="1200px">
           <Map zoom={12} center={position} mapId={MAP_KEY}>
-            <AdvancedMarker position={position} onClick={() => setOpen(true)}>
-              <Pin borderColor="black" glyphColor={"black"} />
-            </AdvancedMarker>
-            {open && (
-              <InfoWindow
-                position={position}
-                onCloseClick={() => setOpen(false)}
-              >
-                <Text>This is Paris!</Text>
-              </InfoWindow>
-            )}
+            {currentDay.map(({ latitude, longitude, color }) => {
+              return (
+                <AdvancedMarker
+                  position={{ lat: latitude, lng: longitude }}
+                  //onClick={() => setOpen(true)}
+                >
+                  <Pin
+                    background={color}
+                    borderColor="black"
+                    glyphColor={"black"}
+                  />
+                  {/* {open && (
+                    <InfoWindow
+                      position={{ lat: latitude, lng: longitude }}
+                      onCloseClick={() => setOpen(false)}
+                    >
+                      <Text>This is Paris!</Text>
+                    </InfoWindow>
+                  )} */}
+                </AdvancedMarker>
+              );
+            })}
           </Map>
         </Box>
       </Flex>
