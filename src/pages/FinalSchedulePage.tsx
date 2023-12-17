3c5b8415943abbfc2f1e27ea5260d5b3ce8779c5
Keith Cabay <keith.cabay10@gmail.com>
2023-12-17 13:47:12 -0500

    Add Google map

--- src/pages/FinalSchedulePage.tsx
+++ src/pages/FinalSchedulePage.tsx
@@ -1,4 +1,5 @@
 import Header from "../components/Header";
+import MapSchedule from "../components/MapSchedule";
 import ScheduleImageGallery from "../components/ScheduleImageGallery";
 
 const FinalSchedulePage = () => {
@@ -6,6 +7,7 @@ const FinalSchedulePage = () => {
     <div>
       <Header />
       <ScheduleImageGallery />
+      <MapSchedule />
     </div>
   );
 };
